c8c75d1ab925c853f29b40b5abb213db45dd1dc4
Anisa <aagermanova@gmail.com>
2020-09-06 21:32:31 -0700

    fix arrows

--- source/js/script.js
+++ source/js/script.js
@@ -1,8 +1,8 @@
 $(function(){
   $('.banner-section__slider').slick({
     dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../img/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../img/arrow-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 969,
@@ -32,8 +32,8 @@ $(function(){
   $('.product-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="../img/arrow-black-left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="../img/arrow-black-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 1301,
